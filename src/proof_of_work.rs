938552e3052d0edd8ad689197002e23308d079c2
0xZaddyy <oyinoladapo5@gmail.com>
2024-07-19 14:36:23 +0100

    updating the nonce function

--- src/proof_of_work.rs
+++ src/proof_of_work.rs
@@ -5,7 +5,25 @@ pub struct proofofwork {
 
 pub fn run(&self) -> (i64, String) {
     let mut nonce = 0;
-    let mut hash = Vec::
+    let mut hash = Vec::new();
+    println!("mining the block");
+    while nonce < MAX_NONCE {
+        let data = self.prepare_data(nonce);
+        hash = crate::SHA256_digest(data.as_slice());
+        let hash_int = Bigint::from_bytes_be(sign::plus, hash.as_slice());
+
+        if hash_int.lt(self.target.borrow()) {
+            println!("{}" HEXLOWER.encode(hash.as_slice()));
+            break;
+    
+        } else {
+            nonce += 1 ;
+        }
+    }
+    println!();
+    return(nonce,HEXLOWER.encode(hash.as_slice()));
+
+
 }
 
     
