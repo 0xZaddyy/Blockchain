b2b1a3b1944fbff9a28dc9ac68cfdac87342deb9
0xZaddyy <oyinoladapo5@gmail.com>
2024-07-20 12:16:43 +0100

    blocks updates

--- src/block.rs
+++ src/block.rs
@@ -1,3 +1,8 @@
+extern crate bincode;
+use bincode::{serialize, deserialize};
+use crate::{ProofOfWork, Transaction};
+use serde::{Deserialize, Serialize};
+
 pub struct Block {
     timestamp: i64,
     pre_block_hash: string,
@@ -29,5 +34,32 @@ pub fn new_block(pre_block_hash: String, transactions: &[Transactions], height: 
 
 impl Block{
     // Functions related to the Block type can be implemented here
+    pub fn new_block(pre_block_hash: String, transactions: &[Transaction],
+        height: usize) -> Block {
+            let mut block = Block{
+                timestamp: crate::current_timestamp(),
+                pre_block_hash,
+                hash: string::new(),
+                transactions: transactions.to_vec(),
+                nonce: 0,
+                height,
+
+            };
+            let pow = ProofOfWork::new_proof_of_work(block.clone());
+            let (nonce, hash) = pow.run();
+            block.nonce = nonce;
+            block.hash = hash;
+            return block;
+
+        }
+
+        pub fn deserialize(bytes: &[u8]) -> Block {
+            bincode::deserialize(bytes).unwrap()
+        }
+        pub fn serialize(&self) -> Vec<u8> {
+            bincode::serialize(&self).unwrap().to_vec()
+        }
     
     }
+
+    
